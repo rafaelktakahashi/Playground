b621a8ad6ec9ac56662eb2bca5f70623c3c1c3a6
rafaelktakahashi <rafael.k.takahashi@gmail.com>
2017-12-31 08:44:21 -0800

    Hexlife works.

--- javascript/hexlife/hexlife.js
+++ javascript/hexlife/hexlife.js
@@ -4,14 +4,14 @@
 // y is horizontal
 
 // Width in hexagons
-var HEXWIDTH = 10;
+var HEXWIDTH = 60;
 
 // Height in hexagons
-var HEXHEIGHT = 10;
+var HEXHEIGHT = 50;
 
 // Radius is actually the distance between center and vertex
 // For a hexagon, that's the same thing as the length of an edge.
-var HEXRADIUS = 30;
+var HEXRADIUS = 10;
 
 // Objects backing the grids
 var mainGrid = null;
@@ -27,8 +27,26 @@ var canvasContext = null;
 var SQRT3 = Math.sqrt(3);
 
 // rules for birth and survival
-var B = [3];
+var B = [2];
 var S = [3,4];
+
+// functions to check if a number matches a rule
+function matchesB(number) {
+    for (var i = 0; i < B.length; i++) {
+        if (B[i] == number) {
+            return true;
+        }
+    }
+    return false;
+}
+function matchesS(number) {
+    for (var i = 0; i < S.length; i++) {
+        if (S[i] == number) {
+            return true;
+        }
+    }
+    return false;
+}
 
 // timer
 var intervalId = null;
@@ -42,13 +60,15 @@ window.onload = function() {
     // 1.5 * side overcounts one;
     // then add side/2 for first and for last
     canvas.height = Math.floor(
-        1.5 * (HEXHEIGHT - 1) * HEXRADIUS + HEXRADIUS
+        1.5 * (HEXHEIGHT - 1) * HEXRADIUS + 2 * HEXRADIUS
     );
 
     // to calculate the width of a single hexagon, we do
-    // sqrt(3)/2 times height
+    // sqrt(3) times radius
+    // add half width to compensate for offset rows
     canvas.width = Math.floor(
-        HEXWIDTH * (HEXRADIUS * 2) * SQRT3
+        HEXWIDTH * HEXRADIUS * SQRT3
+        + HEXRADIUS * SQRT3/2
     );
 
     // Instantiate new grids
@@ -62,22 +82,76 @@ window.onload = function() {
 
         // figure out which hex was clicked and toggle it
         var q = (x * SQRT3/3 - y / 3) / HEXRADIUS;
-        r = y * 2/3 / HEXRADIUS;
-        // q and r are in axial coordinates, and need
-        // to be rounded to the nearest hex.
+        var r = (y * 2/3) / HEXRADIUS;
+        // q and r are in fractional axial coordinates
 
-        var xHex = 2;
-        var yHex = 2;
-        // TODO
+        // round q and r to the nearest hex
+        var clickCoord
+            = cube_to_oddr(
+                cube_round({
+                    x: q,
+                    y: 0 - q - r,
+                    z: r
+                })
+            );
 
-        mainGrid.getCell(xHex, yHex).toggle();
-        // also ask just that cell to redraw
-        mainGrid.getCell(xHex, yHex).drawOnCanvas();
+        var xHex = clickCoord.x;
+        var yHex = clickCoord.y;
+        
+        // try to get the cell
+        var cell = mainGrid.getCell(xHex, yHex);
+
+        if (cell != null) {
+            cell.toggle();
+            // toggle will also update colors
+            // including neighbors
+        }
+
     }, false);
     
     // draw the initial grid
-    canvasContext.strokeStyle = "#00ccff";
+    canvasContext.strokeStyle = "#4080b0";
     mainGrid.drawOnCanvas();
+}
+
+// function that converts cube coordinates to odd-r coordinates
+function cube_to_oddr(cube) {
+    var col = cube.x
+        + (cube.z - (cube.z & 1))
+        / 2;
+    col = col - (cube.z & 1 ? 0 : 1);
+    var row = cube.z - 1;
+    return {
+        x: col,
+        y: row
+    }
+}
+
+// function that rounds a floating-point cube coordinate
+function cube_round(cube) {
+    var rx = Math.round(cube.x);
+    var ry = Math.round(cube.y);
+    var rz = Math.round(cube.z);
+
+    var x_diff = Math.abs(rx - cube.x);
+    var y_diff = Math.abs(ry - cube.y);
+    var z_diff = Math.abs(rz - cube.z);
+
+    if (x_diff > y_diff && x_diff > z_diff) {
+        rx = 0 -ry -rz;
+    }
+    else if (y_diff > z_diff) {
+        ry = 0 -rx -rz;
+    }
+    else {
+        rz = 0 -rx -ry;
+    }
+
+    return {
+        x: rx,
+        y: ry,
+        z: rz
+    };
 }
 
 function start() {
@@ -118,29 +192,53 @@ function Cell(x, y, parent) {
         // generation, regardless of whether it's alive now.
         var nNeighbors = parent.countLiveNeighbors(this.x, this.y);
 
-        var arrayToCompare = null;
-
         if (this.alive) {
             // if this is alive, it needs to survive
-            arrayToCompare = S;
+            return matchesS(nNeighbors);
         }
         else {
             // if this is dead, it needs to be born
-            arrayToCompare = B;
+            return matchesB(nNeighbors);
         }
 
-        for (var i = 0; i < arrayToCompare.length; i++) {
-            if (arrayToCompare[i] == nNeighbors) {
-                return true;
+    }
+
+    // invert this cell
+    // then update this and its neighbors
+    this.toggle = function() {
+        this.alive = !this.alive;
+
+        for (var offsetX = -1; offsetX <= 1; offsetX++) {
+            for (var offsetY = -1; offsetY <= 1; offsetY++) {
+
+                // for odd-rows:
+                if (this.y&1) {
+                    // skip top-left and bottom-left
+                    if (offsetX == -1 && offsetY != 0)
+                    {continue;}
+                }
+                else {
+                    // for even-rows:
+                    // skip top-right and bottom-right
+                    if (offsetX == 1 && offsetY != 0)
+                    {continue;}
+                }
+
+                // update the cell
+                newX = this.x + offsetX;
+                newY = this.y + offsetY;
+                if (newX < 0 || newX >= HEXWIDTH
+                || newY < 0 || newY >= HEXHEIGHT) {
+                    continue;
+                }
+
+                this.parent.getCell(newX, newY).drawOnCanvas();
+
             }
         }
 
-        return false;
-    }
+        
 
-    // method for convenience
-    this.toggle = function() {
-        this.alive = !this.alive;
     }
 
     // draws this cell on the global canvas
@@ -170,16 +268,25 @@ function Cell(x, y, parent) {
             canvasContext.lineTo(point.x, point.y);
         }
         canvasContext.closePath();
-        
-        // if this cell is alive, fill it with black
-        // otherwise, fill it with white
+
+        var nNeighbors = parent.countLiveNeighbors(this.x, this.y);
         if (this.alive) {
-            canvasContext.fillStyle = '#000000';
+            
+            if (matchesS(nNeighbors))
+            {canvasContext.fillStyle = "#002200";}
+            else
+            {canvasContext.fillStyle = "#440000";}
+
             canvasContext.fill();
             canvasContext.stroke();
         }
         else {
-            canvasContext.fillStyle = '#ffffff';
+
+            if (matchesB(nNeighbors))
+            {canvasContext.fillStyle = "#e0ffe0";}
+            else
+            {canvasContext.fillStyle = "#ffffff";}
+
             canvasContext.fill();
             canvasContext.stroke();
         }
@@ -218,7 +325,11 @@ function Grid() {
     // cells:
     this.cells = makeGrid(this);
 
+    // this method returns null if indices are out of bounds.
     this.getCell = function(x, y) {
+        if (x >= HEXWIDTH || y >= HEXHEIGHT) {
+            return null;
+        }
         return this.cells[x][y];
     }
 
@@ -232,9 +343,19 @@ function Grid() {
                 // skip center
                 if (offsetX == 0 && offsetY == 0)
                 {continue;}
-                // skip top-left and bottom-left
-                if (offsetX == -1 && offsetY != 0)
-                {continue;}
+
+                // for odd-rows:
+                if (y&1) {
+                    // skip top-left and bottom-left
+                    if (offsetX == -1 && offsetY != 0)
+                    {continue;}
+                }
+                else {
+                    // for even-rows:
+                    // skip top-right and bottom-right
+                    if (offsetX == 1 && offsetY != 0)
+                    {continue;}
+                }
 
                 var newX = x + offsetX;
                 var newY = y + offsetY;
