8907998b258481c8e1875be000366acb89047e47
Rafael Takahashi <rafael.k.takahashi@gmail.com>
2018-05-10 20:38:18 -0300

    Fixes the Elixir modpow.

--- elixir/modpow.exs
+++ elixir/modpow.exs
@@ -3,12 +3,11 @@ defmodule Modpow do
         tail_calculate(b,e,m,1)
     end
     
-    def tail_calculate(_,0,_,_) do
-        1
-    end
+    def tail_calculate(_,0,_,acc), do: acc
     
     def tail_calculate(b, e, m, acc) do
-        tail_calculate(b, e-1, m, rem(acc*b,m))
+        remainder = rem(acc * b, m)
+        tail_calculate(b, e-1, m, remainder)
     end
 end
 
