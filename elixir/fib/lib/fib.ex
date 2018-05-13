d18672054d053e7d6c844925370cc22e6b861163
Rafael Kenji Takahashi <rafael.k.takahashi@gmail.com>
2018-05-12 21:48:58 -0300

    Got the tail-recursive Fibonacci in Elixir working.

--- elixir/fib/lib/fib.ex
+++ elixir/fib/lib/fib.ex
@@ -2,12 +2,21 @@ defmodule Fib do
   @moduledoc """
   Trivial Fibonacci, just because
   I wanted to see how Elixir Mixes
-  work.
+  work. There's both a naive and a
+  better, tail-recursive function.
   """
 
-  def naivefib(0), do: 1
+  def naivefib(0), do: 0
   def naivefib(1), do: 1
   def naivefib(n) do
     naivefib(n-1) + naivefib(n-2)
+  end
+
+  def fib(0), do: 0
+  def fib(1), do: 1
+  def fib(n), do: tailfib(n-1, 0, 1)
+  def tailfib(0, acc1, acc2), do: acc2
+  def tailfib(n, acc1, acc2) do
+    tailfib(n - 1, acc2, acc1 + acc2)
   end
 end
--- elixir/fib/test/fib_test.exs
+++ elixir/fib/test/fib_test.exs
@@ -3,7 +3,17 @@ defmodule FibTest do
   doctest Fib
 
   test "test naive fib" do
-    assert Fib.naivefib(10) == 55
-    assert Fib.naivefib(16) == 987
+    assert Fib.naivefib(11) == 89
+    assert Fib.naivefib(17) == 1597
+    assert Fib.naivefib(23) == 28657
+  end
+
+  test "test fib" do
+    assert Fib.fib(11) == 89
+    assert Fib.fib(17) == 1597
+    assert Fib.fib(23) == 28657
+    assert Fib.fib(42) == 267914296
+    assert Fib.fib(141) == 131151201344081895336534324866
+    assert Fib.fib(500) == 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125
   end
 end
